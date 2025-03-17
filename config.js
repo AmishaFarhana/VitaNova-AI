cecd9f5547ee9d9fbb5820f7e43233844dfa58fa
Amisha Farhana Shaik <amishafarhana992@gmail.com>
2025-03-17 10:35:08 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+// Environment configuration for EcoScan application
+
+(function() {
+    // Environment variables
+    const envConfig = {
+        // Your Perplexity API key
+        PERPLEXITY_API_KEY: ''
+    };
+    
+    // Make configuration available globally
+    window.ENV = envConfig;
+})();
